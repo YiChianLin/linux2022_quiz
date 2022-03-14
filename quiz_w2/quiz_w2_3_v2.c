ae330d6506909a6246b72b05e03d15f43565e9a1
YiChianLin <heydanny316@hotmail.com>
2022-03-14 17:41:02 +0800

    Modify the context

--- quiz_w2/quiz_w2_3_v2.c
+++ quiz_w2/quiz_w2_3_v2.c
@@ -1,27 +1,29 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdint.h>
 
-uint64_t gcd64(uint64_t u, uint64_t v) {
-  if (!u || !v)
-    return u | v;
-  int shift;
-  for (shift = 0; !((u | v) & 1); shift++) {
-    u /= 2, v /= 2;
-  }
-  while (!(u & 1))
-    u /= 2;
-  do {
-    while (!(v & 1))
-      v /= 2;
-    if (u < v) {
-      v -= u;
-    } else {
-      uint64_t t = u - v;
-      u = v;
-      v = t;
-    }
-  } while (v);
-  return u << shift;
+uint64_t gcd64(uint64_t u, uint64_t v)
+{
+    if (!u || !v) return u | v;
+    int u_ctz = __builtin_ctz(u);
+    int v_ctz = __builtin_ctz(v);
+    int shift = u_ctz < v_ctz ? u_ctz : v_ctz;
+
+    u >>= u_ctz;
+    v >>= v_ctz;
+    
+    do {
+        v >>= __builtin_ctz(v);
+        
+        if (u < v) {
+            v -= u;
+        } else {
+            uint64_t t = u - v;
+            u = v;
+            v = t;
+        }
+    } while (v);
+    return u << shift;
 }
 
 int main() {
